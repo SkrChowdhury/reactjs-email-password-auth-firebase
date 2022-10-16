5fa947d2ff2974c32b16ec92f38c0a8e29b92222
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-17 00:41:58 +0600

    email password authentication done

--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,52 +1,90 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  updateProfile,
+} from 'firebase/auth';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import app from '../firebase/firebase.init';
 import { Link } from 'react-router-dom';
+import app from '../firebase/firebase.init';
 
 const auth = getAuth(app);
 
 const RegisterReactBootstrap = () => {
   const [passwordError, setPasswordError] = useState('');
   const [success, setSuccess] = useState(false);
+
   const handleRegister = (event) => {
     event.preventDefault();
     setSuccess(false);
+
     const form = event.target;
+    const name = form.name.value;
     const email = form.email.value;
     const password = form.password.value;
+    console.log(name, email, password);
+
+    // validate password
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setPasswordError('Please provide at least two uppercase');
+      return;
+    }
+    if (password.length < 6) {
+      setPasswordError('Please should be at least 6 characters.');
+      return;
+    }
+    if (!/(?=.*[!@#$&*])/.test(password)) {
+      setPasswordError('Please add at least one special character');
+      return;
+    }
+    setPasswordError('');
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
         console.log(user);
         setSuccess(true);
         form.reset();
+        verifyEmail();
+        updateUserName(name);
       })
       .catch((error) => {
-        console.error('error:', error);
+        console.error('error', error);
         setPasswordError(error.message);
       });
-    console.log(email, password);
-
-    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
-      setPasswordError('Please Provide at least two upper case');
-      return;
-    }
-    if (password.length < 6) {
-      setPasswordError('Password should be at least 6 characters');
-      return;
-    }
-    if (!/(?=.*[!@#$&*])  /) {
-      setPasswordError('Please add at least one special character');
-      return;
-    }
-    setPasswordError('');
   };
+
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      alert('Please check your email and verify your email address.');
+    });
+  };
+
+  const updateUserName = (name) => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then(() => {
+        console.log('display name updated');
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div className="w-50 mx-auto">
-      <h3 className="text-primary">Please Register</h3>
+      <h3 className="text-primary">Please Register !!!</h3>
       <Form onSubmit={handleRegister}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            required
+          />
+        </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -66,16 +104,15 @@ const RegisterReactBootstrap = () => {
             required
           />
         </Form.Group>
-        <p className="text-danger"> {passwordError}</p>
-        {success && <p className="text-success">User created successfully.</p>}
-
+        <p className="text-danger">{passwordError}</p>
+        {success && <p className="text-success">User Created Successfully.</p>}
         <Button variant="primary" type="submit">
-          Submit
+          Register
         </Button>
       </Form>
       <p>
         <small>
-          Already have an account? Please <Link to="/login">Login</Link>
+          Already have an account? Please <Link to="/login">Log in</Link>
         </small>
       </p>
     </div>
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,10 +4,7 @@ import { Link, Outlet } from 'react-router-dom';
 const Main = () => {
   return (
     <div>
-      <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </nav>
+      <h1>My Email Password Authentication</h1>
       <Outlet></Outlet>
     </div>
   );
